028324cdb5e0008bfc8ea75b933688ad6cdd0975
Michail Kurachkin <michail.kurachkin@promwad.com>
2015-10-12 01:45:51 +0300

    desktop: finish development

--- desktop.c
+++ desktop.c
@@ -5,12 +5,14 @@
  *      Author: Michail Kurochkin
  */
 
+#include <avr/wdt.h>
 #include "types.h"
 #include "config.h"
 #include "gpio.h"
 #include "init_hw.h"
+#include "desktop.h"
 
-//struct desktop_timers Desktop_timers;
+volatile struct desktop_timers Desktop_timers;
 
 
 /**
@@ -20,7 +22,7 @@
 void
 desktop_set_power_down(u8 mode)
 {
-    gpio_set_state(&gpio_list[MCU_GPIO_POWER_DOWN], !mode);
+    gpio_set_state(&gpio_list[MCU_GPIO_POWER_DOWN], mode);
 }
 
 
@@ -33,4 +35,63 @@ desktop_set_power_button(u8 mode)
 {
     gpio_set_state(&gpio_list[MCU_GPIO_POWER_BUTTON], mode);
 }
+
+
+/**
+ * Получить состояние питания компьютера
+ */
+int
+desktop_get_power_state(void)
+{
+    return !gpio_get_state(&gpio_list[MCU_GPIO_POWER_STATE]);
+}
+
+/**
+ * Перегрузить сервер
+ * Функция завершится только при появлении питания сервера
+ */
+void
+desktop_reboot(void)
+{
+    bool desktop_power_state = OFF;
+
+    for (;;) {
+        desktop_set_power_down(ON);
+        Desktop_timers.power_down = CONFIG_DELAY_POWER_DOWN;
+        while (Desktop_timers.power_down > 1) {
+            wdt_reset();
+        }
+        desktop_set_power_down(OFF);
+        Desktop_timers.power_down = 0;
+
+        Desktop_timers.wait_power = CONFIG_DELAY_WAIT_POWER;
+        while (Desktop_timers.wait_power > 1) {
+            desktop_power_state = desktop_get_power_state();
+            if (desktop_power_state) {
+                Desktop_timers.wait_power = 0;
+                return;
+            }
+            wdt_reset();
+        }
+
+        desktop_set_power_button(ON);
+        Desktop_timers.power_down = CONFIG_DELAY_POWER_BUTTON;
+        while (Desktop_timers.power_down > 1) {
+            wdt_reset();
+        }
+        desktop_set_power_button(OFF);
+        Desktop_timers.power_down = 0;
+
+        Desktop_timers.wait_power = CONFIG_DELAY_WAIT_POWER;
+        while (Desktop_timers.wait_power > 1) {
+            desktop_power_state = desktop_get_power_state();
+            if (desktop_power_state) {
+                Desktop_timers.wait_power = 0;
+                return;
+            }
+            wdt_reset();
+        }
+    }
+}
+
 
--- desktop.h
+++ desktop.h
@@ -4,8 +4,19 @@
  *  Created on: 30 янв. 2015 г.
  *      Author: Michail Kurochkin
  */
-#ifndef PUMP_H_
-#define PUMP_H_
+#ifndef DESKTOP_H_
+#define DESKTOP_H_
+
+#include "timers.h"
+
+/**
+ * Таймеры для управления питанием сервера
+ */
+struct desktop_timers {
+    t_counter power_down; // Счетчик отсутсвия питания сервера
+    t_counter wait_power; // Счетчик ожидания появления питания
+};
+extern volatile struct desktop_timers Desktop_timers;
 
 void
 desktop_set_power_down(u8 mode);
@@ -13,5 +24,11 @@ desktop_set_power_down(u8 mode);
 void
 desktop_set_power_button(u8 mode);
 
+int
+desktop_get_power_state(void);
 
-#endif /* PUMP_H_ */
+void
+desktop_reboot(void);
+
+
+#endif /* DESKTOP_H_ */

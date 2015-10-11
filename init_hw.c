0f784809cd8687c0e7f6f7fa84a8fb85c5b86d2c
Michail Kurachkin <michail.kurachkin@promwad.com>
2015-10-12 01:43:28 +0300

    init: finish initialization

--- init_hw.c
+++ init_hw.c
@@ -9,11 +9,11 @@
 #include <avr/wdt.h>
 #include <avr/interrupt.h>
 #include "timers.h"
-#include "serial.h"
 #include "timers.h"
 #include "gpio.h"
 #include "config.h"
 #include "init_hw.h"
+#include "uart.h"
 
 
 /**
@@ -37,15 +37,18 @@ struct gpio gpio_list[] = {
         { // MCU_GPIO_POWER_STATE
             .direction_addr = (u8 *) &DDRD,
             .port_addr = (u8 *) &PORTD,
+            .pin_addr = (u8 *) &PIND,
             .pin = 7,
             .direction = GPIO_INPUT,
-            .output_state = 0
+            .output_state = 1
         },
         { // Завершающий элемент
             .direction_addr = NULL,
             .port_addr = NULL,
         },
 };
+
+
 
 
 
@@ -58,7 +61,7 @@ init_hw(void)
     gpio_init_list(gpio_list);
     usart_init();
     timer2_init();
-    //wdt_enable(WDTO_2S); // Включаем вэтчдог
+    wdt_enable(WDTO_2S); // Включаем вэтчдог
     sei();
 }
 

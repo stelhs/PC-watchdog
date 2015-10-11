74d573f821e1c7f06c2f9054c5f0e4e0f833659c
Michail Kurachkin <michail.kurachkin@promwad.com>
2015-10-12 01:45:13 +0300

    config: added all parameters

--- config.h
+++ config.h
@@ -8,4 +8,13 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
+#define CONFIG_DELAY_POWER_DOWN 3000 // Интервал отключения питания сервера
+#define CONFIG_DELAY_WAIT_POWER 5000 // Интервал появления питания после включения сервера
+#define CONFIG_DELAY_POWER_BUTTON 600 // Время удержания кнопки питания сервера
+
+#define CONFIG_DELAY_WAIT_FIRST_PING (1000 * 60 * 5) // Время ожидания загрузки сервера
+#define CONFIG_DELAY_WAIT_PING 10000 // Максимальный интервал между пингами
+#define CONFIG_PING_BYTE 'Y' // Байт интерпретируемый как PING от сервера
+
+
 #endif /* CONFIG_H_ */

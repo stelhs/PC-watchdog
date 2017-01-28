e79b7d3acde28bceec16e43618081d64e936e003
Michail Kurockin <Michail.Kurachkin@promwad.com>
2017-01-28 15:16:02 +0300

    fixed incorrect computed booting timeout

--- config.h
+++ config.h
@@ -12,7 +12,7 @@
 #define CONFIG_DELAY_WAIT_POWER 5000 // Интервал появления питания после включения сервера
 #define CONFIG_DELAY_POWER_BUTTON 600 // Время удержания кнопки питания сервера
 
-#define CONFIG_DELAY_WAIT_FIRST_PING (1000 * 60 * 5) // Время ожидания загрузки сервера
+#define CONFIG_DELAY_WAIT_FIRST_PING (1000L * 60 * 5) // Время ожидания загрузки сервера
 #define CONFIG_DELAY_WAIT_PING 30000 // Максимальный интервал между пингами
 #define CONFIG_PING_BYTE 'Y' // Байт интерпретируемый как PING от сервера
 
